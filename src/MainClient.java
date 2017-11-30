e06de2f000bb9e373bcf17d4b0d414f61bc5c0b5
daviidsilvaa <davidsfernandes2@gmail.com>
2017-11-30 19:34:32 -0200

    Accept Client of network

--- src/MainClient.java
+++ src/MainClient.java
@@ -1,11 +1,17 @@
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class MainClient {
 	public static void main(String[] args) 
 	throws UnknownHostException, IOException{
+		System.out.print("Insert your IP address: ");
+		Scanner scanner = new Scanner(System.in);
+		
 		// Dispara o cliente
-		new Client("127.0.0.1", 12345).execute();
+		new Client(scanner.nextLine(), 12345).execute();
+		
+		scanner.close();
 	}
 }
